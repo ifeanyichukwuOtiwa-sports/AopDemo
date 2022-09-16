6cc85f63db1d2bf0cf9bdf00a070f4a08155c084
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-09-16 22:40:47 +0100

    [AOP DEMO]: Initial commit

---
+++ src/main/java/io/codewithgx/recorddemo/RecordUserOperationsApplication.java
@@ -0,0 +1,13 @@
+package io.codewithgx.recorddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordUserOperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordUserOperationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codewithgx/recorddemo/RecordUserOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codewithgx.recorddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordUserOperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
