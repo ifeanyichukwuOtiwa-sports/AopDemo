004888ccfb8796cf37a543344d232cf464a5c91d
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-09-17 01:20:19 +0100

    [AOP DEMO]: fix test

--- src/test/java/io/codewithgx/recorddemo/ApplicationLogApplicationTests.java
+++ src/test/java/io/codewithgx/recorddemo/ApplicationLogApplicationTests.java
@@ -5,9 +5,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
+@SuppressWarnings("resource")
 class ApplicationLogApplicationTests {
+
+    @Container
+    private static final MySQLContainer<?> mySQLContainer =
+            new MySQLContainer<>(DockerImageName.parse("mysql:8.0.30-oracle"))
+                    .withUsername("root")
+                    .withPassword("123456")
+                    .withDatabaseName("aopDB")
+                    .withExposedPorts(3306);
 
     @Autowired
     protected ApplicationLogApplication application;
@@ -17,4 +33,11 @@ class ApplicationLogApplicationTests {
         assertThat(application).isNotNull();
     }
 
+    @DynamicPropertySource
+    public static void mySqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mySQLContainer::getDriverClassName);
+    }
 }
