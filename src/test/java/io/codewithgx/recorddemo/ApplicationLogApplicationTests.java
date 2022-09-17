5022fecb4ff6ecb4ded0b80fc233fb0847bf3356
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-09-17 01:01:03 +0100

    [AOP DEMO]: fix test

--- src/test/java/io/codewithgx/recorddemo/ApplicationLogApplicationTests.java
+++ src/test/java/io/codewithgx/recorddemo/ApplicationLogApplicationTests.java
@@ -1,13 +1,20 @@
 package io.codewithgx.recorddemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationLogApplicationTests {
 
+    @Autowired
+    protected ApplicationLogApplication application;
+
     @Test
     void contextLoads() {
+        assertThat(application).isNotNull();
     }
 
 }
